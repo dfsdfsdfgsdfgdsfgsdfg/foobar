3c2d567047df00e6f157937a435f48b6f45bcff0
dfsdfsdfgsdfgdsfgsdfg <110134894+dfsdfsdfgsdfgdsfgsdfg@users.noreply.github.com>
2022-07-28 17:25:16 +0200

    Add files via upload

---
+++ Ifs.java
@@ -0,0 +1,14 @@
+public class Ifs {
+
+	private boolean kctf_nope;
+	private boolean someCodition;
+
+	public void test() {
+		if (kctf_nope) {
+			System.out.println("no");
+		}
+		if (someCodition) {
+		}
+	}
+
+}
